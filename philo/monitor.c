3dff96e41ead5c36d651e0b866df188bed5a191f
Hiroki Urabe <hurabe@c2r7s8.42tokyo.jp>
2024-10-18 20:12:08 +0900

    add monitor.c

--- philo/monitor.c
+++ philo/monitor.c
@@ -6,8 +6,30 @@
 /*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 22:01:53 by hurabe            #+#    #+#             */
-/*   Updated: 2024/10/14 22:02:50 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/10/18 20:08:29 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	monitor(t_data *data)
+{
+	int	i;
+
+	if (data->must_eat == 0)
+		return ;
+	start_delay(data->start_time);
+	while (1)
+	{
+		i = 0;
+		while (i < data->philos_num)
+		{
+			if (check_anyone_dead(data, i))
+				return ;
+			i++;
+		}
+		if (everyone_is_full(data))
+			return ;
+		usleep(300);
+	}
+}
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 20:05:25 by hurabe            #+#    #+#             */
-/*   Updated: 2024/10/17 23:47:22 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/10/18 20:10:52 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,9 @@ int		validate(int argc, char **argv);
 
 // init_data
 int		init_data(t_data *data, int argc, char **argv);
+
+// monitor
+void	monitor(t_data *data);
 
 // time
 void	start_delay(time_t start_time);
